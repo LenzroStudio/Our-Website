045c5e21cbd36d78b242e17e56a12b3b8a7d6bd8
LenzroStudio <team@lenzro.com>
2025-07-17 23:52:24 +0300

    forth

--- components/CustomerService.jsx
+++ components/CustomerService.jsx
@@ -17,7 +17,7 @@ const CustomerService = ({openMenu , setmenu}) => {
       <button
         onClick={handleClick}
         aria-label="Contact us" /* screen‑reader label  */
-        className="fixed bottom-8 right-8 rounded-full bg-emerald-500 p-2 active:scale-95 hover:scale-110 transition-all cursor-pointer"
+        className="fixed bottom-8 right-8 rounded-full bg-emerald-500 p-2 active:scale-95 z-40 hover:scale-110 transition-all cursor-pointer"
       >
         <DotLottieReact
           src="https://lottie.host/a09087c0-0b58-4209-885b-235b1e755cb1/Hj3mrWnnV8.lottie"
